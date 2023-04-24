06e97a7d4955b0b1bf07d0f1dada1ce643a7b146
fgonzalez <felmiquiztli@gmail.com>
2023-04-24 13:13:31 -0600

    fsd

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -38,9 +38,7 @@ class ReportsController extends Controller
            if($pdf==0){
                return response()->download(public_path('storage/report/'.$report.$id.'.xlsx'));
            }else{
-            $process = new Process(["soffice --convert-to 'pdf:calc_pdf_Export:{\"SinglePageSheets\":{\"type\":\"boolean\",\"value\":\"true\"}}' ".$report.$id.".xlsx "]);
-            $process->setWorkingDirectory($caminoalpoder.'/storage/report/');
-            
+            $process = new Process(["soffice --convert-to 'pdf:calc_pdf_Export:{\"SinglePageSheets\":{\"type\":\"boolean\",\"value\":\"true\"}}' ".$report.$id.".xlsx "],$caminoalpoder.'/storage/report/');
             $process->run();
             if (!$process->isSuccessful()) {
                 throw new ProcessFailedException($process);
