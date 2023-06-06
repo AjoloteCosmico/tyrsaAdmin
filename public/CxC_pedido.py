4d21dac736e4a5e69a7e49576e44e29174886d92
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2023-06-05 18:51:53 -0600

    safas

--- public/CxC_pedido.py
+++ public/CxC_pedido.py
@@ -314,7 +314,7 @@ for i in range(0,len(pedidos)):
    worksheet.write('O'+row_index, "{:.2f}".format((pedidos['total'].values[i]- cobros.loc[cobros['order_id']==pedidos['id'].values[i],'amount'].sum())*100/pedidos['subtotal'].values[i])+"%", blue_content)
    #facturado
    if(pedidos['coin_id'].values[i]==1):
-      worksheet.write('P'+row_index, pedidos['total'].values[i]-(facturas.loc[facturas['order_id']==pedidos['id'].values[i],'amount'].sum()-creditos.loc[creditos['order_id']==pedidos['id'].values[i],'amount'].sum()), blue_content)
+      worksheet.write('P'+row_index, (facturas.loc[facturas['order_id']==pedidos['id'].values[i],'amount'].sum()-creditos.loc[creditos['order_id']==pedidos['id'].values[i],'amount'].sum()), blue_content)
       worksheet.write('Q'+row_index, 0, blue_content)
    else:
       worksheet.write('P'+row_index, 0, blue_content)
