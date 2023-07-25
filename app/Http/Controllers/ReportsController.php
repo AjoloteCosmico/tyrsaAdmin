d5b3499cbf68f78861ce6276893b42b95fbb98c4
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2023-07-25 13:29:43 -0600

    af

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -41,18 +41,18 @@ class ReportsController extends Controller
                return response()->download(public_path('storage/report/'.$report.$id.'.xlsx'));
            }else{
             
-            $process2=new Process(['localc','--headless','--convert-to', 'pdf',$report.$id.'.xlsx'],$caminoalpoder.'/storage/report/');
+            $process2=new Process(['localc','--headless','--convert-to', 'pdf', $report.$id.'.xlsx'],$caminoalpoder.'/storage/report/');
             //$process=new Process(['localc','--headless','--convert-to','pdf','--outdir',$caminoalpoder.'/storage/report/',$report.$id.'.xlsx'],$caminoalpoder.'/storage/report/');
             //$process = new Process(["soffice --convert-to 'pdf:calc_pdf_Export:{\"SinglePageSheets\":{\"type\":\"boolean\",\"value\":\"true\"}}' ".$report.$id.".xlsx "],$caminoalpoder.'/storage/report/');
-            $process2->setTimeout(null);
-            $process2->setIdleTimeout(null);
-            $process2->run();
-            if (!$process2->isSuccessful()) {
-                throw new ProcessFailedException($process2);
-            }
-            $data = $process2->getOutput();
+            // $process2->setTimeout(null);
+            // $process2->setIdleTimeout(null);
+            // $process2->run();
+            // if (!$process2->isSuccessful()) {
+            //     throw new ProcessFailedException($process2);
+            // }
+            // $data = $process2->getOutput();
             return response()->download(public_path('storage/report/'.$report.$id.'.pdf'));
-          
+        
         
        }}
     
