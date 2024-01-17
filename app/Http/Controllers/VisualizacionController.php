cee1df97ab72a775a07b317771c785276e5cd8bf
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2024-01-17 10:01:39 -0600

    asf

---
+++ app/Http/Controllers/VisualizacionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\InternalOrder;
+use ArielMejiaDev\LarapexCharts\Facades\LarapexChart;
+
+class VisualizacionController extends Controller
+{
+   public function pedidos_no_autorizados(){
+    // $hoy=now()->modify('-6 hours');
+    $sna1dia=InternalOrder::where('reg_date',now()->modify('-1 day'))->get()->count();
+    $sna2dia=InternalOrder::where('reg_date',now()->modify('-2 day'))->get()->count();
+    $sna3dia=InternalOrder::where('reg_date',now()->modify('-3 day'))->get()->count();
+    $snamasdias=InternalOrder::where('reg_date','<',now()->modify('-3 day'))->get()->count();
+    $grafica_autorizados = LarapexChart::barChart()
+        ->setTitle('Pedidos sin autorizar')
+        ->setSubtitle('por dias desde su emisión')
+         ->addData('pedidos', [$sna1dia,$sna2dia,$sna3dia,$snamasdias])
+         ->setColors(['#2C426C', '#229C32','#8C0C0C'])
+         ->setXAxis(['1 día', '2 dias', '3 dias', '4 o más']);
+
+         return view('visualizacion.index',compact('grafica_autorizados'));
+   }
+
+        
+}
---
+++ resources/views/visualizacion/index.blade.php
@@ -0,0 +1,60 @@
+@extends('adminlte::page')
+
+@section('title', 'Reportes')
+
+@section('content_header')
+    <h1 class="font-bold"><i class="fas fa-bar-chart"></i>&nbsp; CUENTAS POR COBRAR</h1>
+    
+    <h1 class="font-bold">&nbsp; Visualizacion de datos</h1>
+@stop
+
+@section('content')
+<div class="container-flex m-1 bg-gray-300 shadow-lg rounded-lg">
+        <div class="row p-3 m-2 rounded-lg shadow-xl bg-white">
+            
+            <div class="row p-4">
+                <div class="col-sm-12 text-center font-bold text-lg">
+                    <br><br>
+                    
+                               
+                    {!! $grafica_autorizados->container() !!}
+
+                    <table style="width: 60%">
+                        <tr> 
+                            <td> Cosecutivo Pedidos no autorizados</td>
+                            <td>
+                            <a href="{{route('reports.generate',[0,'CxC_cliente_desglosado',0])}}">
+                                  <button class="button"> <span class="badge badge-success">Excel &nbsp; <i class="fa fa-file-excel-o fa-lg" aria-hidden="true"></i></span> </button>
+                                  </a>  
+                               
+                            </td>
+                            <td>
+                            <a href="{{route('reports.generate',[1,'CxC_cliente_desglosado',1])}}">
+                                  <button class="button"> <span class="badge badge-danger">PDF &nbsp;<i class="fa fa-file-pdf-o fa-lg" aria-hidden="true"></i></span> </button>
+                                  </a>  
+                            </td>
+                        </tr>
+                       
+                    </table>
+
+                 <script src="{{ $grafica_autorizados->cdn() }}"></script>
+                {!! $grafica_autorizados->script() !!}
+
+  
+        
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@stop
+
+@section('css')
+    
+@stop
+
+@section('js')
+
+
+@stop
