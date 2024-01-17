198602ccae9027645281d15eb5c3162ce476e6e1
Felipe Iván González Lucas <fgonzalez@pve.unam.mx>
2024-01-17 10:15:49 -0600

    asf

--- app/Http/Controllers/VisualizacionController.php
+++ app/Http/Controllers/VisualizacionController.php
@@ -10,16 +10,16 @@ class VisualizacionController extends Controller
 {
    public function pedidos_no_autorizados(){
     // $hoy=now()->modify('-6 hours');
-    $sna1dia=InternalOrder::where('reg_date',now()->modify('-1 day'))->get()->count();
-    $sna2dia=InternalOrder::where('reg_date',now()->modify('-2 day'))->get()->count();
-    $sna3dia=InternalOrder::where('reg_date',now()->modify('-3 day'))->get()->count();
-    $snamasdias=InternalOrder::where('reg_date','<',now()->modify('-3 day'))->get()->count();
+    $sna1dia=InternalOrder::where('date','>',now()->modify('-2 day'))->where('status','CAPTURADO')->get()->count();
+    $sna2dia=InternalOrder::where('date','>',now()->modify('-4 day'))->where('status','CAPTURADO')->get()->count() -$sna1dia;
+    $sna3dia=InternalOrder::where('date','>',now()->modify('-8 day'))->where('status','CAPTURADO')->get()->count() -$sna1dia -$sna2dia;
+    $snamasdias=InternalOrder::where('date','<',now()->modify('-8 day'))->where('status','CAPTURADO')->get()->count() -$sna1dia -$sna2dia -$sna3dia;
     $grafica_autorizados = LarapexChart::barChart()
         ->setTitle('Pedidos sin autorizar')
         ->setSubtitle('por dias desde su emisión')
          ->addData('pedidos', [$sna1dia,$sna2dia,$sna3dia,$snamasdias])
          ->setColors(['#2C426C', '#229C32','#8C0C0C'])
-         ->setXAxis(['1 día', '2 dias', '3 dias', '4 o más']);
+         ->setXAxis(['1 o 2 día', '3-4', '1 semana dias', 'más antiguos']);
 
          return view('visualizacion.index',compact('grafica_autorizados'));
    }
