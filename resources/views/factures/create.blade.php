3a09805eb1624db061128a6b0f46b09ab0b72d23
AjoloteCosmico <felmiquiztli@gmail.com>
2025-05-05 00:38:16 -0600

    asf

--- resources/views/factures/create.blade.php
+++ resources/views/factures/create.blade.php
@@ -4,6 +4,9 @@
 
 @section('content_header')
     <h1 class="font-bold"><i class="fas fa-file-lines"></i>&nbsp; FACTURA</h1>
+    <!-- Bootstrap 5.0 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+       
 @stop
 
 @section('content')
