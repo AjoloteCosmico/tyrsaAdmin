f17b1c90b053067f9d69094ee8b0fc7464ac2e83
AjoloteCosmico <felmiquiztli@gmail.com>
2025-09-16 12:44:04 -0600

    asf

--- app/Models/comissions.php
+++ app/Models/comissions.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class comissions extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'order_id',
+        'seller_id',
+        'description',
+        'percentage',
+    ];
 }
