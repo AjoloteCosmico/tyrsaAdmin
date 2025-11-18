65496d923940e9dca71287f4aead65330bcf216c
AjoloteCosmico <72818026+AjoloteCosmico@users.noreply.github.com>
2025-11-18 16:07:54 -0600

    asf

--- app/Http/Controllers/InternalOrderController.php
+++ app/Http/Controllers/InternalOrderController.php
@@ -1307,7 +1307,7 @@ public function recalcular_total($id){
         $request->validate([
         'order_id' => 'required|exists:internal_orders,id',
         // 'contrat' debe ser un archivo, de tipo MIME 'pdf' y con un tamaño máximo (ej. 10MB)
-        'contrat' => 'required|file|mimes:pdf|max:100240', 
+        'contrat' => 'required|file|mimes:pdf', 
         'observations' => 'nullable|string',
         ]);
         $InternalOrder=InternalOrder::find($request->order_id);
